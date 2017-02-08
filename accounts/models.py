2a4eeecd753895b0abdf6378c626d98ae5477350
Bruno Henrique de Fabre <bruno.hfabre@gmail.com>
2017-02-08 16:53:48 -0200

    Inicio UserProfile

--- accounts/models.py
+++ accounts/models.py
@@ -3,8 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Team(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField('Nome', max_length=100, unique=True)
+    backgroud = models.CharField('Fundo', max_length=100)
+    front = models.CharField('Frente', max_length=100)
 
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -16,5 +20,15 @@ class Team(models.Model):
         verbose_name_plural = 'Times'
 
 
-class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=CASCADE, unique=True)
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE)
+    ra = models.CharField('RA', max_length=100)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user
+
+    class Meta:
+        verbose_name = 'Perfil'
+        verbose_name_plural = 'Perfis'
--- games/models.py
+++ games/models.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 class Sport(models.Model):
     name = models.CharField('Nome', max_length=100)
     duration_time = models.DecimalField('Duração geral (min)', max_digits=10, decimal_places=0)
@@ -14,8 +15,8 @@ class Sport(models.Model):
         return self.name
 
     class Meta:
-        verbose_name='Esporte'
-        verbose_name_plural='Esportes'
+        verbose_name = 'Esporte'
+        verbose_name_plural = 'Esportes'
 
 
 class Game(models.Model):
