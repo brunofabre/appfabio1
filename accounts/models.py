d627995d9ae15290117a6c2057908b4d2017682c
Bruno Fabre <bruno.hfabre@gmail.com>
2017-02-07 23:16:29 -0200

    começando model perfil

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Team(models.Model):
+    name = models.CharField('Nome', max_length=100, unique=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Time'
+        verbose_name_plural = 'Times'
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=CASCADE, unique=True)
